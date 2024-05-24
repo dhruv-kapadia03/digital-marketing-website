06494c5e077f07472c1c7336456c461832dba347
Dhruv Kapadia <143247270+dhruv-kapadia03@users.noreply.github.com>
2024-05-24 10:51:44 +0530

    updated images, updated all code and added other files.

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-let nav = document.querySelector(".navbar");
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 20) {
-        nav.classList.add("header-scrolled");
-    } else {
-        nav.classList.remove("header-scrolled");
-    }
-}
-
-
-let navBar = document.querySelectorAll(".nav-link")
-let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(a) {
-    a.addEventListener("click", function() {
-        navCollapse.classList.remove("show");
-    })
-})
+let nav = document.querySelector(".header");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
